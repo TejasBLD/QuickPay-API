869cf069068c7687dd18db2bfbf1d9eeaa241e66
Tejas Singh <worktejas2033@gmail.com>
2026-02-03 20:31:36 +0530

    Refactor SECRET_KEY to be set dynamically

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     APP_Name:str="QuickPay API"
     DATABASE_URL:str="sqlite:///./quick.db"
-    SECRET_KEY:str="399f64111591bf578eac21f7761df086"
+    SECRET_KEY:str
     ALGORITHM:str="HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES:int =30
     model_config={
